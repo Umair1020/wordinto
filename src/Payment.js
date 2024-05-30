3c4b127028d1c92c182b229cad149e6b9552b493
Muddassir Shaikh <ktech.muddassir@gmail.com>
2024-05-31 02:14:28 +0500

    Minor Modifications

--- src/Payment.js
+++ src/Payment.js
@@ -61,6 +61,7 @@ const CheckoutForm = ({ card }) => {
       formData.append('cardLast4', paymentMethod.card.last4);
       formData.append('amount', card.price);
       formData.append('selectedType', card.selectedType);
+      formData.append('title', card.title);
 
       // Send data to the server using Axios
       try {
